8de043a493ba3416a9e98e04a984918458d9cbdd
Etienne-Sasenarine <etiennesasenarine@gmail.com>
2026-04-23 17:28:34 -0400

    added finalCompetition.m structure and practice maps

---
+++ finalCompetition.m
@@ -0,0 +1,7 @@
+function [dataStore] = finalCompetition(Robot,maxTime, offset_x, offset_y)
+% main function for AMR final competition
+
+% load map data
+load("practiceMap2026_4credit.mat")
+% load("compMap.mat")
+
